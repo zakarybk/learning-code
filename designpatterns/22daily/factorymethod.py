39f3044417e08fd888c22de68d07e9f68188e596
Zak <19314863+zakarybk@users.noreply.github.com>
2020-06-18 23:56:45 +0100

    Unable to properly convert Factory Method C# example to Python. __init__ is not being called when using abc

--- designpatterns/22daily/factorymethod.py
+++ designpatterns/22daily/factorymethod.py
@@ -1,11 +1,10 @@
-from abc import ABC
+from abc import ABC, ABCMeta, abstractmethod
 
 class Ingredient(ABC):
     pass
 
 class Bread(Ingredient):
     pass
-
 class Turkey(Ingredient):
     pass
 class Lettuce(Ingredient):
@@ -14,5 +13,31 @@ class Mayonnaise(Ingredient):
     pass
 
 
+class Sandwich(ABC):
+   
+    def __init__(self):
+        self.ingredients = self.CreateIngredients()
+        super().__init__() # Not called
+
+    @abstractmethod
+    def CreateIngredients(self):
+        return []
 
 
+class TurkeySandwich(Sandwich):
+    def __init__(self):
+        super().__init__() # Not called
+
+    def CreateIngredients(self):
+        self.ingredients = []
+        self.ingredients.append(Bread())
+        self.ingredients.append(Mayonnaise())
+        self.ingredients.append(Lettuce())
+        self.ingredients.append(Turkey())
+        self.ingredients.append(Turkey())
+        self.ingredients.append(Bread())
+        return len(self.ingredients)
+
+turkeySandwich = TurkeySandwich().CreateIngredients()
+num = turkeySandwich
+print(f"This sandwich has {num} ingredients!")
