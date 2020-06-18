9b830026431e28a8ebee347e8758e50a82f19dce
Zak <19314863+zakarybk@users.noreply.github.com>
2020-06-18 22:22:28 +0100

    Added in design patterns folder

---
+++ designpatterns/22daily/README.md
@@ -0,0 +1,6 @@
+# Following The Daily Design Pattern
+## Learning 22 Software Design Patterns in 22 Days
+## Matthew P Jones
+
+# Requirements
+* Python 3
---
+++ designpatterns/22daily/factorymethod.py
@@ -0,0 +1,18 @@
+from abc import ABC
+
+class Ingredient(ABC):
+    pass
+
+class Bread(Ingredient):
+    pass
+
+class Turkey(Ingredient):
+    pass
+class Lettuce(Ingredient):
+    pass
+class Mayonnaise(Ingredient):
+    pass
+
+
+
+
