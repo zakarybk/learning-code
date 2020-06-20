88e13282ec50529890e3b8464f932c7a21a10b16
Zak <19314863+zakarybk@users.noreply.github.com>
2020-06-20 17:14:19 +0100

    Spaced out yellow and red buttons

--- react-native/ColourCatalog/App.js
+++ react-native/ColourCatalog/App.js
@@ -1,36 +1,38 @@
-import React from "react";
-import {
-    Text,
-    View, 
-    ActivityIndicator, 
-    ProgressBarAndroid, 
-    ProgressViewIOS,
-    Button,
-    Alert,
-    Dimensions
-} from "react-native";
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
 
-const { height, width } = Dimensions.get('window');
+import picMountain from "./assets/gray-asphalt-road-on-cliff.jpg"
 
 export default function App(){
-    const onButtonPress = () => {
-        Alert.alert(`${new Date().toLocaleTimeString()} button pressed`);
-    };
   return(
-    <View style={{ padding: 50 }}>
-      {Platform.OS === "ios" && <ProgressViewAndroid progress={0.5} />}
-      {Platform.OS === "android" && 
-        <ProgressBarAndroid 
-          styleAttr="Horizontal"
-          indeterminate={false}
-          color={"blue"}
-          progress={0.5} />
-      }
-      <ActivityIndicator size="large" color="#61DBFB" />
-      <Button title="click me" onPress={onButtonPress}/>
-      <Text>OS: {Platform.OS}</Text>
-      <Text>Height: {height}</Text>
-      <Text>Width: {width}</Text>
+    <View style={styles.page}>
+      <Text style={[styles.text, styles.selectedText]}>Red</Text>
+      <Text style={styles.text}>Green</Text>
+      <Text style={styles.text}>Blue</Text>
     </View>
-  );
+  )
 } 
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-around",
+    alignItems: "flex-start",
+    marginTop: 40,
+    backgroundColor: "#DDD"
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 22,
+    color: "red",
+    backgroundColor: "yellow",
+    margin: 10,
+    padding: 5
+  },
+  selectedText: {
+    alignSelf: "flex-end",
+    backgroundColor: "red",
+    color: "yellow"
+  }
+})
---
+++ react-native/ColourCatalog/assets/README.md
@@ -0,0 +1,3 @@
+# Credits for photos
+gray-asphalt-road-on-cliff.jpg - Photo by Catherine from Pexels
+
