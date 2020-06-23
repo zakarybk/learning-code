92255f84a29a9accf19679b56fd54e9ccce4b3f6
Zak <19314863+zakarybk@users.noreply.github.com>
2020-06-23 23:38:46 +0100

    User interface for a form

--- react-native/ColourCatalog/App.js
+++ react-native/ColourCatalog/App.js
@@ -2,10 +2,13 @@ import React, { useState } from "react"
 import { StyleSheet, FlatList } from "react-native"
 import ColorButton from "./components/ColorButton"
 import defaultColors from "./data/defaultColors.json"
+import ColorForm from "./components/ColorForm"
 
 export default function App(){
   const [backgroundColor, setBackgroundColor] = useState("blue");
   return(
+    <>
+    <ColorForm></ColorForm>
     <FlatList style={[styles.container, { backgroundColor }]}
       data={defaultColors}
       renderItem={({ item }) => {
@@ -18,6 +21,7 @@ export default function App(){
         );
       }}
     />
+    </>
   );
 } 
 
