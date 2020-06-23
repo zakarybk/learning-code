638d5823c2ec9e9db4ae72dcf3ebba02093c9207
Zak <19314863+zakarybk@users.noreply.github.com>
2020-06-23 23:07:21 +0100

    Yellow button + highlight

--- react-native/ColourCatalog/App.js
+++ react-native/ColourCatalog/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { StyleSheet, Text, View, Image, Dimensions } from "react-native"
+import { StyleSheet, Text, View, TouchableHighlight } from "react-native"
 
 import picMountain from "./assets/gray-asphalt-road-on-cliff.jpg"
 import picCave from "./assets/abstract-antelope-canyon-art-blur.jpg"
@@ -8,12 +8,14 @@ export default function App(){
   const [backgroundColor, setBackgroundColor] = useState("blue");
   return(
     <View style={[styles.container, { backgroundColor }]}>
-      <Text style={styles.button}
-        onPress={() => setBackgroundColor("green")}
-        >green</Text>
-      <Text style={styles.button}
-        onPress={() => setBackgroundColor("red")}
-        >red</Text>
+      <TouchableHighlight style={styles.button}
+        onPress={() => setBackgroundColor("yellow")}
+        underlayColor="orange">
+        <View style={styles.row}>
+          <View style={[styles.sample, { backgroundColor: "yellow" }]} />
+          <Text style={styles.buttonText}>yellow</Text>
+        </View>
+      </TouchableHighlight>
     </View>
   )
 } 
@@ -26,8 +28,25 @@ const styles = StyleSheet.create({
     alignItems: "center"
   },
   button: {
-    fontSize: 30,
     margin: 10,
-    padding: 10
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 10,
+    alignSelf: "stretch",
+    backgroundColor: "rgba(255,255,255,0.8)"
+  },
+  buttonText: {
+    fontSize: 30,
+    textAlign: "center"
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  sample: {
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+    backgroundColor: "white"
   }
 })
