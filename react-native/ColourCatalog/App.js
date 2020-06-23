12dd781f11b230aed48eda7050a51518172163fe
Zak <19314863+zakarybk@users.noreply.github.com>
2020-06-23 22:59:43 +0100

    Buttons to change background colour using useState hooking

--- react-native/ColourCatalog/App.js
+++ react-native/ColourCatalog/App.js
@@ -1,28 +1,33 @@
-import React from "react"
+import React, { useState } from "react"
 import { StyleSheet, Text, View, Image, Dimensions } from "react-native"
 
 import picMountain from "./assets/gray-asphalt-road-on-cliff.jpg"
 import picCave from "./assets/abstract-antelope-canyon-art-blur.jpg"
 
 export default function App(){
+  const [backgroundColor, setBackgroundColor] = useState("blue");
   return(
-    <View style={styles.page}>
-      <Image style={styles.image} source={picMountain} />
-      <Image style={styles.image} source={picCave} />
+    <View style={[styles.container, { backgroundColor }]}>
+      <Text style={styles.button}
+        onPress={() => setBackgroundColor("green")}
+        >green</Text>
+      <Text style={styles.button}
+        onPress={() => setBackgroundColor("red")}
+        >red</Text>
     </View>
   )
 } 
 
 const styles = StyleSheet.create({
-  page: {
+  container: {
     flex: 1,
+    display: "flex",
     justifyContent: "center",
-    alignItems: "center",
+    alignItems: "center"
   },
-  image: {
-    flex: 1,
-    borderRadius: 50,
+  button: {
+    fontSize: 30,
     margin: 10,
-    width: Dimensions.get("window").width - 10
-  },
+    padding: 10
+  }
 })
