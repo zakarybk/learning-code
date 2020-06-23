a686c3d63b80b5b099781bb2ab2b971ca7bf3690
Zak <19314863+zakarybk@users.noreply.github.com>
2020-06-23 22:24:15 +0100

    Added look function and helpers

--- lisp/landoflisp/text-game-engine.lisp
+++ lisp/landoflisp/text-game-engine.lisp
@@ -16,13 +16,45 @@
                         (attic (living-room downstairs ladder))))
 
 (defun describe-path (edge)
-  '(there is a ,(caddr edge) going, (cadr edge) from here.))
+  `(there is a ,(caddr edge) going, (cadr edge) from here.))
 
 (describe-path '(garden west door))
 
 (defun describe-paths (location edges)
-  (apply #'append (mapcar #'describe-path (cdr (assoc location edges))))
+  (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
 
 (describe-paths 'living-room *edges*)
 
+(defparameter *objects* '(whiskey bucket frog chain))
 
+(defparameter *object-locations* '((whiskey living-room)
+                                   (bucket living-room)
+                                   (chain garden)
+                                   (frog garden)))
+(defun objects-at (loc objs obj-locs)
+    (labels ((at-loc-p (obj)
+                (eq (cadr (assoc obj obj-locs)) loc)))
+      (remove-if-not #'at-loc-p objs)))
+
+(defun describe-objects (loc objs obj-loc)
+  (labels ((describe-obj (obj)
+                `(you see a ,obj on the floor.)))
+    (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc)))))
+
+(defparameter *location* 'living-room)
+
+(defun look ()
+  (append (describe-location *location* *nodes*)
+          (describe-paths *location* *edges*)
+          (describe-objects *location* *objects* *object-locations*)))
+
+(defun walk (direction)
+  (let ((next (find direction
+                    (cdr (assoc *location* *edges*))
+                    :key #'cadr)))
+    (if next
+      (progn (setf *location* (car next))
+             (look))
+      '(you cannot go that way.))))
+
+    
