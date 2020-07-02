e33f5c7f07151a2000e72e9518a908698d9562cf
Zak <19314863+zakarybk@users.noreply.github.com>
2020-07-02 22:36:13 +0100

    Lisp print + prin1 printing

---
+++ lisp/landoflisp/print.lisp
@@ -0,0 +1,11 @@
+(progn (print "this")
+       (print "is")
+       (print "a")
+       (print "test"))
+       
+(progn (prin1 "this")
+      (prin1 "is")
+      (prin1 "a")
+      (prin1 "test"))
+
+
