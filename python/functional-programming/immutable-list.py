d23e47f0b9f772b5c6616d34e887702f25206d59
Zak <19314863+zakarybk@users.noreply.github.com>
2020-07-02 12:01:49 +0100

    Immutable scientist, but in mutable list

---
+++ python/functional-programming/immutable-list.py
@@ -0,0 +1,17 @@
+import collections
+from pprint import pprint
+
+Scientist = collections.namedtuple('Scientist', [
+    'name',
+    'field',
+    'born',
+    'nobel'
+])
+
+scientists = [
+    Scientist(name='Ada Lovelace', field='math', born=1815, nobel=False),
+    Scientist(name='Emmy Noether', field='math', born=1882, nobel=False)
+]    
+print(scientists)
+
+
