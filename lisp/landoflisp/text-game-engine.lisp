f7f97c1b1acb90a7fd7f9d0d47bfffea13fb948e
Zak <19314863+zakarybk@users.noreply.github.com>
2020-07-02 23:22:52 +0100

    Lisp, text formatting for cases in sentences

--- lisp/landoflisp/text-game-engine.lisp
+++ lisp/landoflisp/text-game-engine.lisp
@@ -68,5 +68,45 @@
   (cons 'items- (objects-at 'body *objects* *object-locations*)))
 
 (defun game-repl ()
-	(loop (print (eval (read)))))
+	(let ((cmd (game-read)))
+		(unless (eq (car cmd) 'quit))
+			(game-print (game-eval cmd))
+			(game-repl)))
+
+(defun game-read ()
+	(let ((cmd (read-from-string 
+					(concatenate 'string "(" (read-line) ")"))))
+	(flet ((quote-it (x)
+						(list 'quote x)))
+		(cons (car cmd) (mapcar #'quote-it (cdr cmd))))))
+
+(defparameter *allowed-commands* '(look walk pickup inventory))
+
+(defun game-eval (sexp)
+	(if (member (car sexp) *allowed-commands*)
+		(eval sexp)
+		'(i do not know that command.)))
+
+(defun tweak-text (1st caps lit)
+	(when 1st
+	(let ((item (car 1st))
+			(rest (cdr 1st)))
+	(cond ((eql item #\space) (cons item (tweak-text rest caps lit)))
+		((member item '(#\! #\? #\.)) (cons item (tweak-text rest t lit)))
+		((eql item #\") (tweak-text rest caps (not lit)))
+		(lit (cons item (tweak-text rest nil lit)))
+		(caps (cons (char-upcase item) (tweak-text rest nil lit)))
+		(t (cons (char-downcase item) (tweak-text rest nil nil)))))))
+
+(defun game-print (1st)
+	(princ (coerce (tweak-text (coerce (string-trim "() "
+														(prin1-to-string 1st))
+										'list)
+								t
+								nil)
+					'string))
+	(fresh-line))
+
+
+
 (game-repl)
