e91bf9e94afdcea52a4e27add80b533c3ccc2b4b
Zak <19314863+zakarybk@users.noreply.github.com>
2020-07-03 14:31:09 +0100

    Python, reduce to sum all ages - two different ways

---
+++ python/functional-programming/reduce.py
@@ -0,0 +1,30 @@
+import collections
+from functools import reduce
+from pprint import pprint
+
+Scientist = collections.namedtuple('Scientist', [
+    'name',
+    'field',
+    'born',
+    'nobel'
+])
+
+scientists = (
+    Scientist(name='Ada Lovelace', field='math', born=1815, nobel=False),
+    Scientist(name='Emmy Noether', field='math', born=1882, nobel=False),
+    Scientist(name='Marie Curi', field='physics', born=1867, nobel=True)
+)    
+
+def filter_name_and_age(x):
+    return {'name': x.name, 'age': 2020 - x.born}
+
+names_and_ages = tuple(map(filter_name_and_age, scientists))
+
+total_age = reduce(
+        lambda acc, val: acc + val['age'],
+        names_and_ages,
+        0)
+
+print(total_age)
+
+print(sum(x['age'] for x in names_and_ages))
