c6964e07453b9ca1754e96aec490dc113285f932
Zak <19314863+zakarybk@users.noreply.github.com>
2020-07-03 12:42:07 +0100

    Python, map to filter name and age from list

---
+++ python/functional-programming/map.py
@@ -0,0 +1,22 @@
+import collections
+from pprint import pprint
+
+Scientist = collections.namedtuple('Scientist', [
+    'name',
+    'field',
+    'born',
+    'nobel'
+])
+
+scientists = (
+    Scientist(name='Ada Lovelace', field='math', born=1815, nobel=False),
+    Scientist(name='Emmy Noether', field='math', born=1882, nobel=False),
+    Scientist(name='Marie Curi', field='physics', born=1867, nobel=True)
+)    
+
+def filter_name_and_age(x):
+    return {'name': x.name, 'age': 2020 - x.born}
+
+names_and_ages = tuple(map(filter_name_and_age, scientists))
+print(names_and_ages)
+
