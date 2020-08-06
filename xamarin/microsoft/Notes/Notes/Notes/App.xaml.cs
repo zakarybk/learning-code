c159c27f18cdfc2d3b1a9fe5024ed8a93398fb7b
zakarybk <19314863+zakarybk@users.noreply.github.com>
2020-08-06 14:24:52 +0100

    Working note adding following - https://docs.microsoft.com/en-us/xamarin/get-started/quickstarts/multi-page?pivots=windows

--- xamarin/microsoft/Notes/Notes/Notes/App.xaml.cs
+++ xamarin/microsoft/Notes/Notes/Notes/App.xaml.cs
@@ -1,16 +1,17 @@
 ﻿using System;
+using System.IO;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace Notes
 {
     public partial class App : Application
     {
+        public static string FolderPath { get; private set; }
         public App()
         {
             InitializeComponent();
-
-            MainPage = new MainPage();
+            FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+            MainPage = new NavigationPage(new NotesPage());
         }
 
         protected override void OnStart()
