a46918300b74819e8683cec72f61037698b0533d
zakarybk <19314863+zakarybk@users.noreply.github.com>
2020-08-07 20:21:41 +0100

    Increment and decrement counter in Xamarin following https://www.youtube.com/watch?v=5CgQUbnf1Qk

---
+++ xamarin/reso-coder/README.md
@@ -0,0 +1,2 @@
+# Tutorials followed from Reso Coder
+https://www.youtube.com/channel/UCSIvrn68cUk8CS8MbtBmBkA
---
+++ xamarin/reso-coder/XamarinTut/XamarinTut/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.IO;
+
+namespace XamarinTut
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextView txtNumber;
+        int number;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            txtNumber = FindViewById<TextView>(Resource.Id.txtNumber);
+
+            FindViewById<Button>(Resource.Id.btnIncrement).Click += (o, e) => 
+                txtNumber.Text = (++number).ToString();
+
+            FindViewById<Button>(Resource.Id.btnDecrement).Click += (o, e) =>
+                txtNumber.Text = (--number).ToString();
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
