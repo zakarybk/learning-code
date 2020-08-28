19de74a9b6f07c027a5071de727b9050c2e8846c
zakarybk <19314863+zakarybk@users.noreply.github.com>
2020-08-28 14:28:45 +0100

    Changes

---
+++ c#/task-2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace task_2
+{
+    enum Day
+    {
+        MON,
+        TUE,
+        WED,
+        THU,
+        FRI,
+        SAT,
+        SUN
+    }
+
+    class MeetingDateParser
+    {
+        MeetingDateParser(string data)
+        {
+            
+        }
+
+        void GetDay()
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
